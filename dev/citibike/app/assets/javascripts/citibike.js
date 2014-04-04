df2aaef7bf617032ab9b163d409ac486eec1162b
tunneylax20 <bryantunney@gmail.com>
2014-04-04 14:14:00 -0400

    can enter starting point and destination for bicycle directions

--- dev/citibike/app/assets/javascripts/citibike.js
+++ dev/citibike/app/assets/javascripts/citibike.js
@@ -1,37 +1,91 @@
-var directionsDisplay;
-var map;
-
-
-$(document).ready(function() {
-
-var directionsService = new google.maps.DirectionsService();
-
-function initialize() {
-  directionsDisplay = new google.maps.DirectionsRenderer();
-  var new_york = new google.maps.LatLng(40.7284186, -73.98713956);
-  var mapOptions = {
-    zoom: 14,
-    center: new_york
-  }
-  map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-  directionsDisplay.setMap(map);
-}
-
-function calcRoute() {
-  var start = "Elizabeth and Houston, New York, NY";
-  var end = "21st and Broadway";
-  var request = {
-    origin:start,
-    destination:end,
-    travelMode: google.maps.TravelMode.BICYCLING
-  };
-  directionsService.route(request, function(result, status) {
-    if (status == google.maps.DirectionsStatus.OK) {
-      directionsDisplay.setDirections(result);
-    }
+$(document).ready(function(){
+  $('#get-directions-form').on('submit', function(e){
+    e.preventDefault();
+    getDirections();
   });
-} 
-initialize();
-calcRoute();
+})
 
-});
+
+function getDirections(){
+  $(document).ready(function() {
+
+  var directionsService = new google.maps.DirectionsService();
+
+  function initialize() {
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    var new_york = new google.maps.LatLng(40.7284186, -73.98713956);
+    var mapOptions = {
+      zoom: 14,
+      center: new_york
+    }
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    directionsDisplay.setMap(map);
+  }
+
+  function calcRoute() {
+    var start = $('#start').val();
+    var end = $('#end').val();
+    var request = {
+      origin:start,
+      destination:end,
+      travelMode: google.maps.TravelMode.BICYCLING
+    };
+    directionsService.route(request, function(result, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(result);
+      }
+    });
+  } 
+  initialize();
+  calcRoute();
+  });
+}
+  
+
+
+
+
+
+
+
+// $(document).ready(function() {
+//   function initializeMap() {
+//   var directionsDisplay;
+//   var directionsService = new google.maps.DirectionsService();
+//   var map
+//   var directionsDisplay = new google.maps.DirectionsRenderer();
+//   var new_york = new google.maps.LatLng(40.7284186, -73.98713956);
+//   var mapOptions = {
+//     zoom: 14,
+//     center: new_york
+//   }
+//   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+//   directionsDisplay.setMap(map);
+// }
+
+// function calcRoute() {
+//   var directionsDisplay;
+//   var directionsService = new google.maps.DirectionsService();
+//   var map
+//   var start = "Yankee Stadium";
+//   var end = "Elizabeth and Houston, NY";
+//   var request = {
+//     origin:start,
+//     destination:end,
+//     travelMode: google.maps.TravelMode.BICYCLING
+//   };
+//   directionsService.route(request, function(result, status) {
+//     if (status == google.maps.DirectionsStatus.OK) {
+//       directionsDisplay.setDirections(result);
+//     }
+//   });
+// } 
+
+// initializeMap();
+// calcRoute();
+// });
+
+
+
+
+

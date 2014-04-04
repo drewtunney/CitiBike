30d72a5dad822dfc2a0ea88dcd6ffe858ea0963c
tunneylax20 <bryantunney@gmail.com>
2014-04-04 14:41:30 -0400

    old directions removed on new search

--- dev/citibike/app/assets/javascripts/citibike.js
+++ dev/citibike/app/assets/javascripts/citibike.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
   $('#get-directions-form').on('submit', function(e){
+    $('.adp').remove();
     e.preventDefault();
     getDirections();
   });
-})
+});
 
 
 function getDirections(){
@@ -20,6 +21,7 @@ function getDirections(){
     }
     map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
     directionsDisplay.setMap(map);
+    directionsDisplay.setPanel(document.getElementById("directionsPanel"));
   }
 
   function calcRoute() {
@@ -40,52 +42,3 @@ function getDirections(){
   calcRoute();
   });
 }
-  
-
-
-
-
-
-
-
-// $(document).ready(function() {
-//   function initializeMap() {
-//   var directionsDisplay;
-//   var directionsService = new google.maps.DirectionsService();
-//   var map
-//   var directionsDisplay = new google.maps.DirectionsRenderer();
-//   var new_york = new google.maps.LatLng(40.7284186, -73.98713956);
-//   var mapOptions = {
-//     zoom: 14,
-//     center: new_york
-//   }
-//   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-//   directionsDisplay.setMap(map);
-// }
-
-// function calcRoute() {
-//   var directionsDisplay;
-//   var directionsService = new google.maps.DirectionsService();
-//   var map
-//   var start = "Yankee Stadium";
-//   var end = "Elizabeth and Houston, NY";
-//   var request = {
-//     origin:start,
-//     destination:end,
-//     travelMode: google.maps.TravelMode.BICYCLING
-//   };
-//   directionsService.route(request, function(result, status) {
-//     if (status == google.maps.DirectionsStatus.OK) {
-//       directionsDisplay.setDirections(result);
-//     }
-//   });
-// } 
-
-// initializeMap();
-// calcRoute();
-// });
-
-
-
-
-
